fbcaa4c0a2b02250ec1bbf9821954be526a159bd
odiljonswe <karimovodilbek008@gmail.com>
2023-07-11 17:44:13 +0500

    vazifa

---
+++ main.js
@@ -0,0 +1,495 @@
+
+
+function add (x, y, myfunc) {
+    let res = x + y;
+    return myfunc(res);
+}
+
+function newFunc(some) {
+    return console.log(some);
+}
+
+add (3, 4, newFunc);
+
+function  odilbek (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (mone) {
+    return console.log(mone);
+}
+
+ (3, 4, newFunc);
+
+
+ odilbek (3, 54, newFunc);
+
+function  rasul (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social);
+}
+
+ (3, 4, 3, newFunc);
+
+
+ rasul (3, 54, 5, newFunc);
+
+
+ function sardor (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+ }
+
+ function newFunc (social) {
+    return console.log(social);
+ }
+
+ sardor (2, 6, 8, newFunc);
+
+ function adham (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+ }
+
+ function newFunc (title) {
+    return console.log(title)
+ }
+
+ adham (5, 6, 8, newFunc)
+
+ function salim (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+ }
+
+
+ function  newFunc (title) {
+    return console.log(title)
+ }
+
+ salim (5, 9, 8, newFunc)
+
+ 
+ function sayid (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+ }
+ 
+
+ function newFunc (title) {
+    return console.log(title)
+ }
+
+ sayid (4, 5, 9, newFunc)
+
+ function karim (x, y, z, myfunc) {
+    let res = x +x +z;
+    res = myfunc(res);
+ }
+
+ function newFunc (social) {
+    return console.log(social)
+ }
+
+
+ karim (5, 6, 9, newFunc)
+
+
+function ali (x, y, z, myfunc) {
+    let res = x + y + z;
+    ret = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+ali (4, 5, 9, newFunc)
+
+function vali (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log (title)
+}
+vali (4, 9, 6, newFunc)
+
+function gani (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return  console.log (title)
+}
+
+gani (4, 9, 6, newFunc)
+
+
+function raman (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+raman (4, 58, 6, newFunc)
+
+
+function xoji (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+xoji (7,8,92, newFunc)
+
+function otash (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+otash (7,8,2, newFunc)
+
+
+function olim (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+olim (7,8,25, newFunc)
+
+function oltin (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+oltin (8,1,22, newFunc)
+
+function xumo (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function  newFunc (title) {
+    return console.log(title)
+}
+
+xumo  (8, 9, 189, newFunc)
+
+function  dior (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+dior (8, 9, 19, newFunc)
+
+function omad (x, y, z, myfunc)  {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)           
+}
+
+omad (5, 4, 98, newFunc)
+
+
+function rashid (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+} 
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+rashid (5, 45, 15, newFunc)
+
+function ruslan (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+ruslan (9, 4, 15, newFunc)
+
+function alijon (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+alijon (9, 16, 15, newFunc)
+
+function salimjon (x,y,z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+salimjon (6,5,61, newFunc)
+
+function olimbek (x, y, z, myfunc) {
+    let res = x + y + z;
+        res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+olimbek (6,5,1, newFunc)
+
+
+function ganijon (x, y, z, myfunc) {
+    let res = x + y + z;
+            res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+ganijon (6,5,10, newFunc)
+
+function durdon (x, y, z, myfunc) {
+    let res = x + y + z;
+            res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+durdon (23,64,56, newFunc)
+
+
+function durdona (x, y, z, myfunc) {
+    let res = x + y + z;
+            res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+durdona (23,6,51, newFunc)
+
+
+function diyorbek (x, y, z, myfunc) {
+    let res = x + y + z;
+            res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+diyorbek (5, 45, 5, newFunc)
+ 
+function bunyod (x, y, z, myfunc) {
+    let res = x + y + z;
+            res = myfunc(res);
+} 
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+bunyod (14,5,13, newFunc) 
+
+function halimbek (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+halimbek (5, 13, newFunc)
+
+function asl (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title)
+}
+
+asl (5, 42, 7, newFunc)
+
+function asila (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+asila (8, 9, newFunc)
+
+
+function akrom (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+akrom (8, 92, newFunc)
+
+function rahim (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+rahim (8, 93, 45, newFunc)
+
+
+function rahimat  (x, y, z, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+rahimat (8, 93, 45, newFunc)
+
+function rahmatul (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+rahmatul (8, 93, 45, newFunc)
+
+function rahmatillo (x, y, z, myfunc) {
+    let res = x + y + z;
+    res = myfunc(res);
+}
+
+function newFunc (social) {
+    return console.log(social)
+}
+
+rahmatillo (8, 93, 45, newFunc)
+
+function javohir (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title);
+}
+
+javohir (46,446, newFunc)
+
+function javohirbek (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title);
+}
+
+javohirbek (6, 56, newFunc)
+
+function winner (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title);
+}
+
+winner (5,15, newFunc)
+
+function social (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title);
+}
+
+social (78,5, newFunc)
+
+
+function omadbek (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title);
+}
+
+omadbek (78,55, newFunc)
+
+function tilla (x, y, myfunc) {
+    let res = x + y;
+    res = myfunc(res);
+}
+
+function newFunc (title) {
+    return console.log(title);
+}
+
+tilla (78,24, newFunc)
